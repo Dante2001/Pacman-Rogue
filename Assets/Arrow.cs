4dda05cac461e95919662c605ae6971189babc1c
Dante2001 <andrianodia@hotmail.com>
2016-09-21 18:22:18 -0300

    added more items

---
+++ Assets/Arrow.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Arrow : MonoBehaviour {
+
+	public float speed = .004f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+	void FixedUpdate() {
+		Vector3 move = new Vector3(Mathf.Cos(transform.eulerAngles.z*Mathf.Deg2Rad)*speed ,Mathf.Sin(transform.eulerAngles.z*Mathf.Deg2Rad)*speed , 0);
+		//Debug.Log (move.x+" "+move.y + " " + move.z);
+		//Debug.Log (Mathf.Cos (transform.eulerAngles.z * Mathf.Deg2Rad));
+		//Debug.Log (Mathf.Sin(transform.eulerAngles.z*Mathf.Deg2Rad));
+		transform.position = new Vector3(transform.position.x + Mathf.Cos(transform.eulerAngles.z*Mathf.Deg2Rad)*speed ,transform.position.y + Mathf.Sin(transform.eulerAngles.z*Mathf.Deg2Rad)*speed , 0);
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/Bow.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bow : MonoBehaviour {
+
+	public GameObject arrow;
+	float timerPerShot = .5f;
+	float currentTime = 0f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+	void FixedUpdate() {
+		currentTime += Time.fixedDeltaTime;
+		shoot ();
+		//Debug.Log (transform.rotation.x+" "+transform.rotation.y+" "+transform.rotation.z);
+	}
+
+	void shoot() {
+		if (currentTime > timerPerShot) {
+			currentTime = 0;
+			//transform.rotation
+			Instantiate(arrow,transform.position,transform.rotation);
+			Debug.Log (transform.rotation.eulerAngles.x + " " + transform.rotation.eulerAngles.y + " " + transform.rotation.eulerAngles.z);
+		}
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+
+
+}
